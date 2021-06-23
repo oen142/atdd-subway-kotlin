c94f45503d02d060376bc0e46dc2934ff6b63663
Wani <46095454+oen142@users.noreply.github.com>
2021-06-23 17:15:43 +0900

    feat : 회원 생성을 요청하는 step을 만든다. [#1]

---
+++ src/test/kotlin/com/wani/subway/subway/member/step/MemberSteps.kt
@@ -0,0 +1,28 @@
+package com.wani.subway.subway.member.step
+
+import io.restassured.RestAssured
+import io.restassured.response.ExtractableResponse
+import io.restassured.response.Response
+import org.springframework.http.MediaType
+
+object MemberSteps {
+
+    const val email: String = "email@naver.com"
+    const val password: String = "password"
+    const val age: Int = 30
+
+    fun `회원_생성_요청`(email: String, password: String, age: Int): ExtractableResponse<Response> {
+
+        val params: MutableMap<String, String> = HashMap()
+        params["email"] = email
+        params["password"] = password
+        params["age"] = age.toString()
+
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(params)
+            .`when`().post("/members")
+            .then().log().all().extract()
+    }
+}
