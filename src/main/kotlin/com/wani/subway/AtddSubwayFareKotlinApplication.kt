55ee89f18fe1ae96628503ac45df71451bd4f239
Wani <46095454+oen142@users.noreply.github.com>
2021-06-23 15:10:41 +0900

    Initial commit

---
+++ src/main/kotlin/com/wani/subway/AtddSubwayFareKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.wani.subway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtddSubwayFareKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtddSubwayFareKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/wani/subway/AtddSubwayFareKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wani.subway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtddSubwayFareKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
